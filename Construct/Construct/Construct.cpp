42eb883b4330db9cfe36512905c750da7dde9e49
KnoxaramaV2 <woodrowtpscott@gmail.com>
2025-11-16 12:59:19 -0600

    Project init

---
+++ Construct/Construct/Construct.cpp
@@ -0,0 +1,12 @@
+﻿// Construct.cpp : Defines the entry point for the application.
+//
+
+#include "Construct.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
